b23abaf620ef91e58a0339f46a1051f2d8119533
shining-ai <tderkkwii+social@gmail.com>
2023-11-03 15:09:41 +0000

    A58を追加

--- kyopro_tessoku/chap08/A58.py
+++ kyopro_tessoku/chap08/A58.py
@@ -4,54 +4,57 @@ import sys
 
 def debug_input():
     _INPUT = """\
-    8 4
-    1 3 16
-    2 4 7
-    1 5 13
-    2 4 7
+8 4
+1 3 16
+2 4 7
+1 5 13
+2 4 7
     """
     sys.stdin = io.StringIO(_INPUT)
 
 
-def update(RMQ, pos, x, size):
-    RMQ[pos + size] = x
-    while pos > 0:
-        RMQ[pos] = max(RMQ[pos * 2], RMQ[pos * 2 + 1])
-        pos //= 2
+class segtree:
+    def __init__(self, n):
+        self.size = 1
+        while self.size < n:
+            self.size *= 2
+        self.RMQ = [0] * (2 * self.size)
 
-    return RMQ
+    def update(self, pos, x):
+        pos_suffix = pos + self.size
+        self.RMQ[pos_suffix] = x
+        while pos_suffix > 0:
+            pos_suffix //= 2
+            self.RMQ[pos_suffix] = max(
+                self.RMQ[pos_suffix * 2], self.RMQ[pos_suffix * 2 + 1]
+            )
+
+    def show_max(self, L, R, a, b, u):
+        if R <= a or b <= L:
+            return -1000000000
+        if L <= a and b <= R:
+            return self.RMQ[u]
+
+        m = (a + b) // 2
+        answerl = self.show_max(L, R, a, m, u * 2)
+        answerr = self.show_max(L, R, m, b, u * 2 + 1)
+        return max(answerl, answerr)
 
 
-def show_max(RMQ, L, R, a, b, u):
-    if R <= a or b <= 1:
-        return -1000000000
-    if L <= a and b <= R:
-        return RMQ[u]
-
-    m = (a + b) // 2
-    answerl = show_max(RMQ, L, R, a, m, u * 2)
-    answerr = show_max(RMQ, L, R, m, b, u * 2 + 1)
-    return max(answerl, answerr)
-
-
-# O
+# O(logN)
 def main():
     N, Q = map(int, input().split())
-
-    size = 1
-    while size < N:
-        size *= 2
-    RMQ = [0] * (2 * size)
+    Z = segtree(N)
 
     for _ in range(Q):
         Query, L, R = map(int, input().split())
         if Query == 1:
-            RMQ = update(RMQ, L, R, size)
-        elif Query == 2:
-            answer = show_max(RMQ, L - 1, R - 1, 0, size, 1)
+            Z.update(L - 1, R)
+        if Query == 2:
+            answer = Z.show_max(L - 1, R - 1, 0, Z.size, 1)
             print(answer)
 
 
 if __name__ == "__main__":
-    debug_input()
+    # debug_input()
     main()
