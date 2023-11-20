4b7e9069e24858c2a28fa4102d9e815bb8ee9df1
shining-ai <tderkkwii+social@gmail.com>
2023-11-20 15:30:05 +0000

    A20を分かりやすく修正

--- kyopro_tessoku/chap04/A20.py
+++ kyopro_tessoku/chap04/A20.py
@@ -13,28 +13,27 @@ kyoto
 
 # 計算量: O(ST)
 def main():
-    S = input()
-    T = input()
+    S = " " + input()
+    T = " " + input()
 
     s_num = len(S)
     t_num = len(T)
 
-    dp = [[0] * (t_num + 1) for _ in range(s_num + 1)]
+    dp = [[0] * (t_num) for _ in range(s_num)]
 
-    for i_s in range(s_num + 1):
-        for j_t in range(t_num + 1):
-            if (S[i_s - 1] == T[j_t - 1]) and i_s >= 1 and j_t >= 1:
+    for i_s in range(1, s_num):
+        for j_t in range(1, t_num):
+            if S[i_s] == T[j_t]:
                 dp[i_s][j_t] = max(
                     dp[i_s - 1][j_t],
                     dp[i_s][j_t - 1],
                     dp[i_s - 1][j_t - 1] + 1,
                 )
-            elif i_s >= 1 and j_t >= 1:
-                dp[i_s][j_t] = max(dp[i_s - 1][j_t], dp[i_s][j_t - 1])
-            elif i_s >= 1:
-                dp[i_s][j_t] = dp[i_s - 1][j_t]
-            elif j_t >= 1:
-                dp[i_s][j_t] = dp[i_s][j_t - 1]
+            else:
+                dp[i_s][j_t] = max(
+                    dp[i_s - 1][j_t],
+                    dp[i_s][j_t - 1],
+                )
 
     print(dp[-1][-1])
 
